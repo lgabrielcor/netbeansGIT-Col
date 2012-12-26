00e3c4b04f9f6638b51e596789e310a071342769
lgabrielcor <lgabrielcor@gmail.com>
2012-12-26 10:26:39 -0500

    primera version

---
+++ Goliat/src/java/com/login/SesionLogin.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.login;
+
+import java.io.Serializable;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author GABRIELCORREDOR
+ */
+@SessionScoped
+public class SesionLogin implements Serializable {
+
+    /**
+     * Creates a new instance of SesionLogin
+     */
+    public SesionLogin() {
+    }
+}
